302705e0330b410bae18dbbf9c58c875cab1c5fd
hack4code <code4hack@gmail.com>
2019-02-17 19:42:52 +0800

    start-server

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM pypy:3
+
+RUN mkdir /s5p/
+COPY requirements.txt /s5p/
+COPY utils.py /s5p/
+COPY server.py /s5p/
+
+WORKDIR /s5p/
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
+CMD ["pypy3", "server.py"]
--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,23 @@
-# /usr/bin/env bash
+#! /usr/bin/env bash
 
+NAME="s5p"
+DOCKER="docker"
+GREP="grep"
 
-DOCKER=docker
+if !($DOCKER image ls --format '{{.Repository}}' | $GREP "$NAME" &>/dev/null)
+then
+	echo "build image ..."
+	$DOCKER build -t $NAME .
+fi
 
-$DOCKER container run --restart=always --name s5p -v `pwd`:/s5p/ -w /s5p/ -p 8080:8080 -d pypy ./pypy.sh
+if !($DOCKER container ls -a --format '{{.Names}}' | $GREP "$NAME" &>/dev/null)
+then
+	echo "create container ..."
+	$DOCKER run -d --restart=always --name $NAME -p 8080:8080 -v `pwd`/keys/:/s5p/keys/ $NAME
+fi
+
+if !($DOCKER container ls --format '{{.Names}}' | $GREP "$NAME" &>/dev/null)
+then
+	echo "start container ..."
+	$DOCKER container start "$NAME"
+fi
