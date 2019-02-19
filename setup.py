d1f63877d3f5b49457fea96296fa61b40cdc7c05
hack4code <code4hack@gmail.com>
2019-02-19 20:49:49 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='s54http',
+    version='1.0.0',
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    description='socks5 proxy',
+    packages=find_packages(),
+    install_requires=[
+        'cryptography',
+        'pycrypto',
+        'pyOpenSSL',
+        'service-identity',
+        'Twisted',
+    ],
+    entry_points="""
+    [console_scripts]
+    s5pproxy = s54http.proxy:main
+    s5pserver = s54http.server:main
+    """
+)
