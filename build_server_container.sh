3bf27eeb951fcbf8229fb9ee8925dee4dd24cfc3
hack4code <code4hack@gmail.com>
2019-02-23 21:28:17 +0800

    rm container image before build

--- build_server_container.sh
+++ build_server_container.sh
@@ -5,20 +5,20 @@ NAME="s5p"
 GREP="grep"
 DOCKER="docker"
 
-if !($DOCKER image ls --format '{{.Repository}}' | $GREP "$NAME" &>/dev/null)
+
+if ($DOCKER container ls -a --format '{{.Names}}' | $GREP "$NAME" &>/dev/null)
 then
-	echo "build image ..."
-	$DOCKER build -t $NAME .
+	echo "rm container ..."
+	$DOCKER container rm "$NAME"
 fi
 
-if !($DOCKER container ls -a --format '{{.Names}}' | $GREP "$NAME" &>/dev/null)
+if ($DOCKER image ls --format '{{.Repository}}' | $GREP "$NAME" &>/dev/null)
 then
-	echo "create container ..."
-	$DOCKER run -d --restart=always --name $NAME -p 8080:8080 $NAME
+	echo "rm image ..."
+	$DOCKER image rm "$NAME"
 fi
 
-if !($DOCKER container ls --format '{{.Names}}' | $GREP "$NAME" &>/dev/null)
-then
-	echo "start container ..."
-	$DOCKER container start "$NAME"
-fi
+echo "build image ..."
+$DOCKER build -t $NAME .
+echo "build container ..."
+$DOCKER run -d --restart=always --name $NAME -p 8080:8080 $NAME
