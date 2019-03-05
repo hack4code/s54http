ba3d74d84fe8967fa08e85c70e42bdb8b18d7c4e
hack4code <code4hack@gmail.com>
2019-03-05 15:55:06 +0800

    refactor

--- build_container.sh
+++ build_container.sh
@@ -2,8 +2,6 @@
 
 
 NAME="s5p"
-GREP="grep"
-DOCKER="docker"
 
 
 if (( $# < 1 )); then
@@ -22,27 +20,27 @@ if [[ $ROLE == "proxy" ]]; then
 fi
 
 
-if ($DOCKER container ls -a --format '{{.Names}}' | $GREP "$NAME" &>/dev/null)
+if (docker container ls -a --format '{{.Names}}' | grep "$NAME" &>/dev/null)
 then
 	echo "rm container ..."
-	$DOCKER container stop "$NAME"
-	$DOCKER container rm "$NAME"
+	docker container stop "$NAME"
+	docker container rm "$NAME"
 fi
 
-if ($DOCKER image ls --format '{{.Repository}}' | $GREP "$NAME" &>/dev/null)
+if (docker image ls --format '{{.Repository}}' | grep "$NAME" &>/dev/null)
 then
 	echo "rm image ..."
-	$DOCKER image rm "$NAME"
+	docker image rm "$NAME"
 fi
 
 echo "build image ..."
-$DOCKER build -t $NAME .
+docker build -t $NAME .
 
 echo "build $ROLE container ..."
 if [[ "$ROLE" == "server" ]]; then
-	$DOCKER create --restart=always --name $NAME -p 8080:8080 $NAME s5pserver
+	docker create --restart=always --name $NAME -p 8080:8080 $NAME s5pserver
 elif [[ "$ROLE" == "proxy" ]]; then
-	$DOCKER create --restart=always --name $NAME -p 127.0.0.1:8080:8080 $NAME s5pproxy -l 0.0.0.0 -S $SERVER
+	docker create --restart=always --name $NAME -p 127.0.0.1:8080:8080 $NAME s5pproxy -l 0.0.0.0 -S $SERVER
 else
 	echo "only support server|proxy container"
 	exit 1
